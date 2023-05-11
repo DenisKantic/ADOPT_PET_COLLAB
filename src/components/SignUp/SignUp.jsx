056bfbe2874cc538c4759c5fdf74ef0974fb703b
Bego96 <begic.ismar@gmail.com>
2023-05-11 19:36:34 +0200

    Made responside design for Sing Up form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -4,15 +4,15 @@ function SingUp() {
 
     return (
         <div className="h-screen flex items-center justify-center">
-            <div className="bg-slate-300 w-[600px] box-border py-[40px] mt-[100px] flex-column items-center justify-center relative">
-                <div className="w-[350px] mx-auto absolute top-30 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="bg-slate-300 w-[30%] mx-auto box-border p-[2%] flex items-center justify-center relative 2xl:w-[40%] lg:w-[50%] md:w-[70%] md:p-[4%]">
+                <div className="w-[65%] mx-auto absolute top-[10px] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     <img src={image} alt="man_with_dog" className="w-[full]"/>
                 </div>
-                <form id="signUpForm" className="w-[500px] mx-auto mt-[220px]">
-                    <input type="text" placeholder="Name" className="block mb-[20px] w-[500px] p-[5px]"/>
-                    <input type="text" placeholder="Email" className="block mb-[20px] w-[500px] p-[5px]"/>
-                    <input type="text" placeholder="Phone Number" className="block mb-[20px] w-[500px] p-[5px]"/>
-                    <input type="text" placeholder="Password" className="block mb-[20px] w-[500px] p-[5px]"/>
+                <form id="signUpForm" className="mt-[40%] w-[100%]">
+                    <input type="text" placeholder="Name" className="block mb-[20px] w-[100%] p-[5px]"/>
+                    <input type="text" placeholder="Email" className="block mb-[20px] w-[100%] p-[5px]"/>
+                    <input type="text" placeholder="Phone Number" className="block mb-[20px] w-[100%] p-[5px]"/>
+                    <input type="text" placeholder="Password" className="block mb-[20px] w-[100%] p-[5px]"/>
                     <div className="flex my-[40px] items-center justify-center">
                         <button type="submit" className="bg-orange-700 text-white w-[100px] h-[40px] mx-[30px]">Sign Up</button>
                         <span className="mx-[30px] text-red-500">Cancel</span>
